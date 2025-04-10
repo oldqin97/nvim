83d5828850a23d72e78c2b68bea6917b2d9e99ba
oq <oldqin1997@gmail.com>
2025-04-10 21:31:14 +0800

    feat(styled-components/lspconfig): 增加styled-componets高亮和语法补全

---
+++ lua/plugins/lsp/styled-components.lua
@@ -0,0 +1,17 @@
+-- styled-components syntax highlight and completion support for neovim's built-in LSP client
+
+-- styled-components syntax highlight support treesitter install styled
+return {
+  "neovim/nvim-lspconfig",
+  opts = function(_, opts)
+    -- styled-components completion support
+    LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
+      {
+        name = "@styled/typescript-styled-plugin",
+        -- location = "/Users/qin/.volta/tools/image/node/22.13.1/lib/node_modules",
+        -- location = LazyVim.get_pkg_path("@styled/typescript-styled-plugin", "/node_modules/@vue/language-server"),
+        enableForWorkspaceTypeScriptVersions = true,
+      },
+    })
+  end,
+}
