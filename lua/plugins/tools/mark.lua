52ba2b7d2116a739e193370c60b48648c00973fe
oq <oldqin1997@gmail.com>
2025-04-22 20:06:42 +0800

    fix(mark): 修复书签标记隔离bug

--- lua/plugins/tools/mark.lua
+++ lua/plugins/tools/mark.lua
@@ -35,9 +35,23 @@ return {
     },
 
     config = function()
+      -- 1. 读取当前目录并清理末尾多余斜杠
+      local cwd = vim.fn.getcwd():gsub("/+$", "") -- 把 "/foo/bar///" 这种多斜杠情况统一为 "/foo/bar"
+
+      -- 2. 从 cwd 里提取项目名
+      local project_name = vim.fn.fnamemodify(cwd, ":t") -- 直接取尾部目录名称
+
+      -- 3. 构造并创建保存书签的目录
+      local bookmarks_dir = vim.fn.stdpath("data") .. "/bookmarks"
+      vim.fn.mkdir(bookmarks_dir, "p") -- 递归创建，类似 `mkdir -p`
+
+      -- 4. 最终拼出项目专属的文件路径
+      local save_file = string.format("%s/%s.json", bookmarks_dir, project_name)
+
       require("bookmarks").setup({
-        save_file = vim.fn.expand("~/.config/nvim/.bookmarks"),
-        numhl = true,
+        -- save_file = vim.fn.expand("~/.config/nvim/.bookmarks"),
+        save_file = save_file,
+        -- numhl = true,
         linehl = true,
         signs = {
           add = {
